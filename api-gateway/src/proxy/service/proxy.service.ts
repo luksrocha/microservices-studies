85c4121f706a5639ff3cfb83570b07be66934de5
Lucas Rocha <lucasrocha94.dev@gmail.com>
2026-08-25 18:01:23 -0300

    adding the proxy service to capture services health

--- api-gateway/src/proxy/service/proxy.service.ts
+++ api-gateway/src/proxy/service/proxy.service.ts
@@ -1,9 +1,72 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
+import { serviceConfig } from '../../config/gatewa.config';
+import { UserInfo } from '../../types/user-info';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ProxyService {
   private readonly logger = new Logger(ProxyService.name);
 
   constructor(private readonly httpService: HttpService) {}
+
+  async proxyRequest(
+    serviceName: keyof typeof serviceConfig,
+    method: string,
+    path: string,
+    data?: any,
+    headers?: any,
+    userInfo?: UserInfo,
+  ) {
+    const service = serviceConfig[serviceName];
+    const url = `${service.url}${path}`;
+
+    this.logger.log(`Proxying ${method} request to ${serviceName}: ${url}`);
+
+    try {
+      const enhancedHeaders = {
+        ...headers,
+        'x-user-id': userInfo?.userId,
+        'x-user-email': userInfo?.email,
+        'x-user-role': userInfo?.role,
+      };
+
+      const response = await firstValueFrom(
+        this.httpService.request({
+          method: method.toLocaleLowerCase() as any,
+          url,
+          data,
+          headers: enhancedHeaders,
+          timeout: service.timout,
+        }),
+      );
+
+      return response;
+    } catch (error) {
+      this.logger.error(
+        `Error proxying ${method} request to ${serviceName}: ${url}`,
+      );
+      throw error;
+    }
+  }
+
+  async getServiceHealth(serviceName: keyof typeof serviceConfig) {
+    try {
+      const service = serviceConfig[serviceName];
+
+      const response = await firstValueFrom(
+        this.httpService.get(`${service.url}/health`, {
+          timeout: 3000,
+        }),
+      );
+
+      return { status: 'healthy', data: response.data };
+    } catch (error) {
+      return {
+        status: 'unhealthy',
+        error: error instanceof Error ? error.message : 'Unknown error',
+      };
+    }
+  }
 }
